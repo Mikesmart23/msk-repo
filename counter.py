e37e9d7b29ee41e3a0d2d38d579510041af3b6d5
[Mikesmart23] <[test@email.com]>
2024-12-18 14:24:39 +0100

    new commit

---
+++ counter.py
@@ -0,0 +1,47 @@
+import os, time
+
+
+def count():
+    typeOfCount = input(        
+'''
+    ____________________________________________________
+
+                        Counting Script 
+    ____________________________________________________
+
+    - For : 
+    
+        >> Ascend Counting Write The Command [ coun -a ]
+
+        >> Descend Counting Write The Command [ coun -d ] 
+
+        >> ''')
+
+    if typeOfCount == 'coun -a':
+        ascending()
+    elif typeOfCount == 'coun -d':
+        descending()
+
+def ascending():
+    print('\n- Ascending Counting Script ...')
+    start = int(input('\n- Set Start Time  !!\n\n>> '))
+    end = int(input('\n- Set Time End  !!\n\n>> '))
+    while end > start:
+        os.system('cls')
+        for count in range(start, end):
+            print(count)
+            time.sleep(1)
+            os.system('cls')
+            start += 1
+def descending():
+    print('\n- Descending Counting Script ...')
+    start = int(input('\n- Set Time By Seconds !!\n\n>> '))
+    while start > 0:
+        os.system('cls')
+        print(start)
+        time.sleep(1)
+        os.system('cls')
+        start -= 1
+
+if __name__ == '__main__':
+    count()
