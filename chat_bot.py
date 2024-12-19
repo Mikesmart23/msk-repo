5354281f6bc60e405bf3d61a8603314d7def82b3
Said Khalil <119082842+Mikesmart23@users.noreply.github.com>
2024-12-19 14:36:02 +0100

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -5,12 +5,9 @@ class ChatBot:
     def run(self):
         bot = [
             'Welcome User, Enter Your Name To Comunicate Better :',
-            'OK , How Can I Help You ?',
-            'So, Your Name Ali 🙄 ??',
-            'Sorry, I Made A Mistake Forgive Man !! ',
-            'Tell Me About Yourself',
-            'They Calls Me Stupid Bot Is That Right ?',
             'How Old Are You ??',
+            'OK , How Can I Help You ?',
+            'Tell Me About Yourself',
             'Do You Have A Hobby ??',
             'Im Advicing You For Doing Some Sport And Healthy Food For Better Well'
         ]
