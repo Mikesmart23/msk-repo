81fa335d9079c422dcc4c356b884969fa93f4a0c
[Mikesmart23] <[test@email.com]>
2024-12-20 18:10:23 +0100

    update

--- maths.py
+++ maths.py
@@ -1,15 +1,44 @@
-
+import os
 class MathOperation:
     def __init__(self):
         pass
 
     def multipliTable(self):
-        number = int(input('- multiplication [ x ] table of ( ? ) number : \n\n-> '))
+        number = int(input('\n- multiplication [ x ] table of which number ( ? )  : \n\n-> '))
         max_range = int(input('\n- set the max range of the operation : '))
+        os.system('cls')
         for i in range(0, max_range):
             print('_'*50)
             print(f'- {number} x {i} =', number * i)
+
+    def subTable(self):
+        
+        number = int(input('\n- substriction [ - ] table of which number ( ? )  : \n\n-> '))
+        max_range = int(input('\n- set the max range of the operation : '))
+        os.system('cls')
+        for i in range(0, max_range):
+            print('_'*50)
+            print(f'- {number} - {i} =', number - i)
+        
+
+    def load(self):
+        select = input(
+'''
+
+    ** Table Selection :
+
+    1) Multiplication Table
+
+    2) substriction Table
+
+
+
+    >> ''')
+        if select == '1':
+            MathOperation().multipliTable()
+        elif select == '2':
+            MathOperation().subTable()
            
 
 
-MathOperation().multipliTable()
+MathOperation().load()
