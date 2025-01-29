2fe7fb1e6b97458c09e587461c936139f6e526b3
[Mikesmart23] <[test@email.com]>
2025-01-29 15:31:43 +0100

    new commit

--- bool_var.py
+++ bool_var.py
@@ -1,2 +1,18 @@
 # Boolean Variable
 
+# + - * 
+
+# < > <= >= == !=
+
+myAge = 34
+yourAge = 230
+
+if myAge < yourAge:
+    print('im younger !!')
+elif myAge > yourAge:
+    print('im older')
+elif myAge != yourAge:
+    print('we are not the same !!')
+
+else:
+    print('no condition is true')
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,1 @@
-'''
-_____________________________________________________________
-
-? - Select Translation Methode By Its Index
-_____________________________________________________________
-
-Translate To :
-
-1 - English -> from ( Auto Detect ) - to ("en" -> English )
-
-2 - Arabic -> from( Auto Detect ) - to ("ar" -> Arabic)
-
-3 - French -> from ( Auto Detect ) - to ("fr" -> French)
-
-4 - Spanish -> from ( Auto Detect ) - to ("es" -> Spanish)
-
-# - ''' )
+myAge != yourAge
